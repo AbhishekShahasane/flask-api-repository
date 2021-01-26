b6e5a8ebce96c24ffa888f8bfc83b594060941bd
abhishahasane <abhishahasane@gmail.com>
2021-01-27 05:14:18 +0530

    Fixed not importing db

--- app.py
+++ app.py
@@ -4,19 +4,13 @@ from flask_jwt import JWT
 from security import authenticate, identity
 from resources.item import Item, ItemList
 from resources.user import UserRegister
-from resources.store import Store,StoreList
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'abhi'
 api = Api(app)
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 
 jwt = JWT(app, authenticate, identity)
 
